bb28bc92bc57f7e20da9a97dc93d03ee204d6c68
myjohnson23 <myjohnson@gmail.com>
2015-07-13 16:33:52 -0500

    Added Contact Me form

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -2,4 +2,32 @@ $(document).ready(function() {
 
   console.log("All resources are loaded");
 
+  // contact submit event handler
+  $('#form_send').on('click', function() {
+
+    var formdata = app.createFormObject();
+    console.log('Clicked form submit....');
+
+  });
+
+
 });
+
+//Namespacing
+var app = app || {};
+app.createFormObject = function() {
+
+  var retJson = {};
+
+  retJson.userName = $('#user_name').val();
+  retJson.userEmail = $('#user_email').val();
+  retJson.userTelephone = $('#user_telephone').val();
+  retJson.eventDate = $('#event_date').val();
+  retJson.eventType = $('#event_type').val();
+  retJson.eventSize = $('#event_size').val();
+  retJson.userRequest = $('#user_request').val();
+
+  console.log(retJson);
+
+  return retJson;
+}
