77ea2a18a9ed85079cfd23a3fde036e3505bfcdd
myjohnson23 <myjohnson@gmail.com>
2015-07-14 21:31:45 -0500

    Project 1 Wireframe and Basic Layout

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -2,18 +2,27 @@ $(document).ready(function() {
 
   console.log("All resources are loaded");
 
-  // contact submit event handler
+  // hide the status divs.  Could also do this via CSS
+
+  $('.loading').hide();
+  $('.success').hide();
+  $('.error').hide();
+
+  // When user clicks the submit button contact submit event handler
   $('#form_send').on('click', function() {
 
     var formdata = app.createFormObject();
+    console.log(formdata);
     console.log('Clicked form submit....');
+    app.sendEmail(formdata);
 
   });
 
 
 });
 
-//Namespacing
+// Namespacing
+
 var app = app || {};
 app.createFormObject = function() {
 
@@ -21,13 +30,43 @@ app.createFormObject = function() {
 
   retJson.userName = $('#user_name').val();
   retJson.userEmail = $('#user_email').val();
-  retJson.userTelephone = $('#user_telephone').val();
-  retJson.eventDate = $('#event_date').val();
-  retJson.eventType = $('#event_type').val();
-  retJson.eventSize = $('#event_size').val();
-  retJson.userRequest = $('#user_request').val();
+  // retJson.userTelephone = $('#user_telephone').val();
+  // retJson.eventDate = $('#event_date').val();
+  // retJson.eventType = $('#event_type').val();
+  // retJson.eventSize = $('#event_size').val();
+  retJson.request = $('#user_request').val();
 
   console.log(retJson);
 
   return retJson;
+}
+app.sendEmail = function(emailData) {
+
+  //display loading info...
+  $('.loading').slideDown(); // or .show()
+
+  // hide old message.... because this is a NEW request
+  $('.success').hide();
+  $('.error').hide();
+
+  // ajax argument
+  var ajaxData = {
+    url: 'http://imperialholonet.herokuapp.com/api/mail',
+    type: 'post',
+    data: emailData,
+    success: function(data) {
+      console.log(data);
+      $('.loading').slideUp(); // or .hide()
+      $('.success').show();  // or .slideDown()
+    },
+    error: function(err) {
+      console.log(err);
+      $('.loading').slideUp() // or .hide()
+      $('.error').show();
+    }
+
+  };
+
+  $.ajax(ajaxData);
+
 }
